5fd080a8231da324c9c2712d45358b93eb246cec
Anh Do <dinhanhdo95@gmail.com>
2018-04-20 22:24:13 +0700

    Add DbContext class

---
+++ Mobile.Models/DAL/MobileDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Mobile.Models.Entities;
+using System.Data.Entity;
+
+namespace Mobile.Models.DAL
+{
+    class MobileDbContext : DbContext
+    {
+        public MobileDbContext() : base("name=MobileConnectionString")
+        {
+        }
+
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<Menu> Menus { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<ProductSpecification> ProductSpecifications { get; set; }
+        public virtual DbSet<Slide> Slides { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasRequired(p => p.Specification)
+                .WithRequiredPrincipal(s => s.Product);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
