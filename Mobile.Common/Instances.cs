f09400ef0ac0c88f50a9907d3de7402841683081
Anh Do <dinhanhdo95@gmail.com>
2018-04-21 12:18:50 +0700

    Add Instances static class

---
+++ Mobile.Common/Instances.cs
@@ -0,0 +1,8 @@
+﻿namespace Mobile.Common
+{
+    public static class Instances
+    {
+        public static string ERROR_STATUS = "error";
+        public static string SUCCESS_STATUS = "ok";
+    }
+}
