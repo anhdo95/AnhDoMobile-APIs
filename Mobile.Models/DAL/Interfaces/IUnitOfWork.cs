3bee2cd50c75c9d72cc1f719ead48c6f9827c144
Anh Do <dinhanhdo95@gmail.com>
2018-04-21 10:38:48 +0700

    Add UnitOfWork pattern

---
+++ Mobile.Models/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mobile.Models.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository ProductRepo { get; }
+        Task SaveAsync();
+    }
+}
---
+++ Mobile.Models/DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using Mobile.Models.DAL.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Mobile.Models.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly MobileDbContext _context;
+        private IProductRepository _productRepo;
+
+        public UnitOfWork(MobileDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProductRepository ProductRepo
+        {
+            get
+            {
+                if (_productRepo == null)
+                    _productRepo = new ProductRepository(_context);
+                return _productRepo;
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                    disposed = true;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // Prevent the finalizer from releasing unmanaged resources that have already been freed by IDisposable.Dispose
+            GC.SuppressFinalize(this);
+        }
+    }
+}
