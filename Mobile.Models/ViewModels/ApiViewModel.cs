8d8d5a2d70c022336a411e820b1098647666b195
Anh Do <dinhanhdo95@gmail.com>
2018-04-21 12:22:18 +0700

    Add ApiViewModel

---
+++ Mobile.Models/ViewModels/ApiViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Mobile.Models.ViewModels
+{
+    public class ApiViewModel
+    {
+        public object References { get; set; }
+        public string Status { get; set; }
+        public string StatusMessage { get; set; }
+        public int Length { get; set; }
+    }
+}
