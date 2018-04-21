1fb3c96cd55a3066b9eed4bfd1a45d0145459e5d
Anh Do <dinhanhdo95@gmail.com>
2018-04-21 22:06:21 +0700

    Add get comments by the product API

---
+++ Mobile.Models/DAL/Repositories/CommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mobile.Models.DAL.Interfaces;
+using Mobile.Models.Entities;
+using System.Linq;
+using Mobile.Models.ViewModels;
+
+namespace Mobile.Models.DAL.Repositories
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+        public CommentRepository(MobileDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<CommentViewModel>> GetCommentsByProductId(int productId)
+        {
+            return await Select(
+                 c => new CommentViewModel
+                 {
+                     Id = c.Id,
+                     ParentId = c.ParentId,
+                     Content = c.Content,
+                     Commentator = c.User.DisplayName,
+                     CreatedDate = c.CreatedDate
+                 },
+                 filter: c => c.Status && c.ProductId == productId,
+                 orderBy: list => list.OrderByDescending(c => c.CreatedDate));
+        }
+    }
+}
---
+++ Mobile.Web/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using Mobile.Common;
+using Mobile.Models.DAL.Interfaces;
+using Mobile.Models.ViewModels;
+using Mobile.Web.Helpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Mobile.Web.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<JsonResult> GetCommentsForProduct(int productId)
+        {
+            string status = Instances.ERROR_STATUS;
+            string statusMessage = string.Empty;
+            var comments = Enumerable.Empty<CommentViewModel>();
+            try
+            {
+                comments = await _unitOfWork.CommentRepo.GetCommentsByProductId(productId);
+                status = Instances.SUCCESS_STATUS;
+            }
+            catch (Exception ex)
+            {
+                statusMessage = ex.Message;
+            }
+            var results = APIHelper.Instance.GetApiResult(new
+            {
+                Comments = comments,
+            }, status, statusMessage, comments.Count());
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
