81493cf9ab8ba005cff9b2b6aca0761b90b54012
Anh Do <dinhanhdo95@gmail.com>
2018-04-21 10:36:48 +0700

    Add Product repository

---
+++ Mobile.Models/DAL/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mobile.Models.Entities;
+
+namespace Mobile.Models.DAL.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        
+    }
+}
---
+++ Mobile.Models/DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mobile.Models.DAL.Interfaces;
+using Mobile.Models.Entities;
+
+namespace Mobile.Models.DAL.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(MobileDbContext context) : base(context)
+        {
+        }
+    }
+}
